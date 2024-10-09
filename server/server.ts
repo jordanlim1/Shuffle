5f2f29183eb39a14670e59b70c58c20f6e4db076
Jordan Lim <jordanlim2015@gmail.com>
2024-10-09 13:40:07 -0400

    set up server

---
+++ server/server.ts
@@ -0,0 +1,36 @@
+import { NextFunction } from 'connect';
+import express, { Request, Response, ErrorRequestHandler } from 'express'
+const app = express();
+import cors from 'cors';
+const PORT = 3000;
+app.use(express.json());
+app.use(cors())
+
+
+app.use("/", (req: Request, res: Response) => {
+    return res.send("the server works!!")
+})
+
+
+//unknown path handler
+app.use("*", (req: Request, res: Response) => {
+    return res.status(404).send("404 page does not exist");
+  });
+
+
+//global error handler
+app.use((err: ErrorRequestHandler , req: Request, res: Response, next: NextFunction) => {
+    const defaultErr = {
+      log: "Express error handler caught unknown middleware error",
+      status: 500,
+      message: { err: "An error occurred" },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
