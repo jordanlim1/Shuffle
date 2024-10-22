d71a44ad24af0dcc0bd772dec9d0e1e8b2c1ee56
jordanlim1 <jordanlim2015@gmail.com>
2024-10-22 15:37:13 -0400

    set up models

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,10 +1,4 @@
 import express, { Request, Response, NextFunction } from "express";
-require("dotenv").config();
-
-const client = process.env.CLIENT_ID;
-const redirectUri = "http://localhost:8080";
-const scope = "user-read-private user-read-email";
-const authUrl = new URL("https://accounts.spotify.com/authorize?");
 
 const authController = {
   authorizeUser: async function (
